1a85d29084e44d19fbecb6d5549ae6d957d00d89
belenkanas <maria.kanas@correo.ucu.edu.uy>
2025-09-15 09:21:01 -0300

    Ej03

--- ej3/ej03.js
+++ ej3/ej03.js
@@ -5,5 +5,40 @@ La función `showRandomDigit` está asociada al click en el display. Al ejecutar
 debe definir un valor aleatorio entre 0 y 9 y mostrar el dígito correspondiente.
 */
 function showRandomDigit() {
-  console.error("¡No se ha implementado la función showRandomDigit! Ver archivo ej03.js.");
+ 
 }
+
+ try{
+    const randomDigit = Math.floor(Math.random() * 10);
+    const display = document.getElementById("displayLED"); 
+    display.textContent = randomDigit;
+    switch(randomDigit) {
+      case 0:
+        document.getElementById("seg-g").style.color = "#030303";
+        break;
+      case 1:
+        document.getElementById("seg-a").style.color = "#030303";
+        document.getElementById("seg-d").style.color = "#030303";
+        document.getElementById("seg-e").style.color = "#030303";
+        document.getElementById("seg-f").style.color = "#030303";
+        document.getElementById("seg-g").style.color = "#030303";
+        break;
+      case 2:
+        document.getElementById("seg-a").style.color = "red";
+        document.getElementById("seg-b").style.color = "red";
+        document.getElementById("seg-d").style.color = "red";
+        document.getElementById("seg-e").style.color = "red";
+        document.getElementById("seg-g").style.color = "red";
+        break;
+      case 3:
+        document.getElementById("seg-a").style.color = "red";
+        document.getElementById("seg-b").style.color = "red";
+        document.getElementById("seg-c").style.color = "red";
+        document.getElementById("seg-d").style.color = "red";
+        document.getElementById("seg-g").style.color = "red";
+        break;
+    }
+  }
+  catch(error){
+  console.error("¡No se ha implementado la función showRandomDigit! Ver archivo ej03.js.");
+  }
